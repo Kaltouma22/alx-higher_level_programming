1aa71cbd73ce33c591348226370eb03669031825
kaltouma <kchbiki363@gmail.com>
2023-11-28 18:29:12 +0100

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for tens in range(9):
     for ones in range(tens + 1, 10):
-        print("{:d}{:d}, ".format(tens, ones), end="")
-print("89")
+        if (tens == 8 and ones == 9):
+            print("89")
+        else:
+            print("{:d}{:d}, ".format(tens, ones), end="")
