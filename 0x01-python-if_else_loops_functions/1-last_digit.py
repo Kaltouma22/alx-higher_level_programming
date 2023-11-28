83e0cf375f5250e7bf98e959e16f58a85cce0d0d
kaltouma <kchbiki363@gmail.com>
2023-11-28 17:24:25 +0100

    print ASCII alphabet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 ldigit = number % 10 if number > 0 else number % -10
 print(f"Last digit of {number} is {ldigit}"
-      , end=" ")
+      ,end=" ")
 if ldigit > 5:
     print("and is greater than 5")
 elif ldigit == 0:
