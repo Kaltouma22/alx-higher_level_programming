9496da127744e32c41557c31373857db5b2e4ed0
kaltouma <kchbiki363@gmail.com>
2023-12-03 16:02:05 +0100

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,3 +3,7 @@ def multiple_returns(sentence):
     if not sentence:
         return (0, None)
     return (len(sentence), sentence[0])
+#Write a function that returns a tuple with the length of a string and its first character.
+#Prototype: def multiple_returns(sentence):
+#If the sentence is empty, the first character should be equal to None
+#You are not allowed to import any module
