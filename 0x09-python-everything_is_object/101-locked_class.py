1ba9ce8cc12af500bdbea16adc8f254c5f6d35d0
Kaltouma22 <kchbiki363@gmail.com>
2024-01-05 20:41:47 +0100

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,7 @@
 """ Define a LockedClass that restricts instance attribute creation """
 
 
-class Lockedclass:
+class LockedClass:
     """
     Lockedclass defines a class that restricts the creation of new
     instance attributes allowing only 'first_name' as a valid attribute
