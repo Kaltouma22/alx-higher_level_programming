becfe2f03beab9e3be81618d9496c89f99298c08
Kaltouma22 <kchbiki363@gmail.com>
2024-01-07 19:48:07 +0100

    done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,26 +3,25 @@
 
 
 def add_integer(a, b=98):
-    """
-    Function to add two integers or floats
+    """ Function to add two integers or floats
 
     Parameters:
-    a (int or float): The first number.
-    b (int or float, optional): The second number. Defaults to 98.
-
-    Returns:
-    int: The addition of a and b as an integer.
+    a: The first number.
+    b: The second number. Defaults to 98.
 
     Raises:
     TypeError: If a or b is not an integer or float.
+
+    Returns:
+    int: The addition of a and b as an integer.
     """
 
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    return int(a + b)
+    return int(a) + int(b)
 
 if __name__ == "__main__":
     import doctest
-    doctest.testfile("test/0-add_integer.txt")
+    doctest.testfile("tests/0-add_integer.txt")
