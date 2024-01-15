ebb2a6097626d166627f9c7a03b82ea951b9de58
Kaltouma22 <kchbiki363@gmail.com>
2024-01-15 16:10:22 +0100

    dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,3 +39,13 @@ class Square(Rectangle):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """Return a dictionary representation of the Rectangle."""
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
+        }
