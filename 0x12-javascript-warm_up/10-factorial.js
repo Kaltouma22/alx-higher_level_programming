205d7990c0e7362b83d31dd451c208be495fc76a
kaltouma22 <kchbiki363@gmail.com>
2024-03-12 12:36:37 +0000

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-function factorial(n) {
-    if (isNaN(n)) {
-        return 1;
-    }
-    if (n <= 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 const arg = process.argv[2];
