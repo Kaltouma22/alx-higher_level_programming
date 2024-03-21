38c58d20bbf8b387c92b809764ba0a37a36a8f07
kaltouma22 <kchbiki363@gmail.com>
2024-03-21 14:57:41 +0000

    done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,34 +5,24 @@ import sys
 
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
     )
 
-    cursor = db.cursor()
+    cursor = conn.cursor()
+    state_name = sys.argv[4]
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s\
+            ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
-    sql = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    try:
-        # Execute the SQL command
-        cursor.execute(sql, (state_name,))
-        # Fetch all the rows in a list of lists
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except Exception as e:
-        print("Error: unable to fetch data")
-        print(e)
+    rows = cursor.fetchall()
 
-    # Disconnect from server
-    db.close()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
