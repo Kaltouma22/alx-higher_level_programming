f7b286576e7ea4b05ccdeacf0f32581c84ee1298
kaltouma22 <kchbiki363@gmail.com>
2024-03-29 00:36:50 +0000

    done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    Script that takes 2 arguments: repository name and owner name
-    Uses the GitHub API to retrieve information about the repository
+    Script that lists 10 commits (from the most recent to oldest)
+    of a given repository by a given owner using the GitHub API
 """
 import sys
 import requests
@@ -11,17 +11,16 @@ if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    url = f"https://api.github.com/repos/{owner_name}/{repository_name}"
+    url = ("https://api.github.com/repos/"
+           f"{owner_name}/{repository_name}/commits")
     response = requests.get(url)
 
     if response.status_code == 200:
-        repo_info = response.json()
-        print(f"Repository name: {repo_info['name']}")
-        print(f"Owner: {repo_info['owner']['login']}")
-        print(f"Description: {repo_info['description']}")
-        print(f"Stars: {repo_info['stargazers_count']}")
-        print(f"Forks: {repo_info['forks_count']}")
-        print(f"URL: {repo_info['html_url']}")
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
     else:
-        print(f"Failed to retrieve repository information.\
-                Status code: {response.status_code}")
+        print(f"Failed to retrieve commits. Status code:\
+                {response.status_code}")
